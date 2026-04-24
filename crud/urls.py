d042c64e8c952c0693bd98e80633f35436b117d6
Rafael Alexandre Guerreiro Nogueira <124052359+rafael-a-g-n@users.noreply.github.com>
2026-04-24 16:28:11 +0000

    URL patterns for CRUD views added

---
+++ crud/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('courses/', views.course_list, name='course_list'),
+    path('courses/create/', views.course_create, name='course_create'),
+    path('courses/<int:pk>/edit/', views.course_update, name='course_update'),
+    path('courses/<int:pk>/delete/', views.course_delete, name='course_delete'),
+]
