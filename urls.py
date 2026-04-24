c93877d8e7c8a31ab269f19b361b8ec717236b79
Rafael Alexandre Guerreiro Nogueira <124052359+rafael-a-g-n@users.noreply.github.com>
2026-04-24 19:40:36 +0000

    Redirect root URL to /courses/ so course list is the default page.

--- urls.py
+++ urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('course_list', permanent=False)),
     path('admin/', admin.site.urls),
     path('', include('crud.urls')),
 ]
