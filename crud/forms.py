fb965fdb5bce30f0321292becb7fb4501bf0f041
Rafael Alexandre Guerreiro Nogueira <124052359+rafael-a-g-n@users.noreply.github.com>
2026-04-25 12:55:27 +0000

    Add LearnerForm to forms.py with validation and security best practices for learner CRUD

--- crud/forms.py
+++ crud/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Instructor
+from .models import Instructor, Learner
 
 class InstructorForm(forms.ModelForm):
     class Meta:
@@ -17,3 +17,21 @@ class InstructorForm(forms.ModelForm):
         if total_learners is not None and total_learners < 0:
             raise forms.ValidationError('Total learners must be non-negative.')
         return total_learners
+
+class LearnerForm(forms.ModelForm):
+    class Meta:
+        model = Learner
+        fields = [
+            'first_name', 'last_name', 'email', 'location', 'dob',
+            'occupation', 'social_link'
+        ]
+        widgets = {
+            'dob': forms.DateInput(attrs={'type': 'date'}),
+            'social_link': forms.URLInput(attrs={'placeholder': 'https://...'}),
+        }
+
+    def clean_social_link(self):
+        link = self.cleaned_data.get('social_link')
+        if link and not link.startswith(('http://', 'https://')):
+            raise forms.ValidationError('Social link must start with http:// or https://')
+        return link
