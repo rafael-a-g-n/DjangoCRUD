aa285c8671085312fc0c90fdd3322c0db2e6e834
Rafael Alexandre Guerreiro Nogueira <124052359+rafael-a-g-n@users.noreply.github.com>
2026-04-25 13:18:57 +0000

    Fix all course template URLs to use 'crud' namespace

--- urls.py
+++ urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.shortcuts import redirect
 
 urlpatterns = [
-    path('', lambda request: redirect('course_list', permanent=False)),
+    path('', lambda request: redirect('crud:course_list', permanent=False)),
     path('admin/', admin.site.urls),
     path('', include(('crud.urls', 'crud'), namespace='crud')),
 ]
